716d27b197decf7fa259304b43040e501dd6173a
bayuevri <bayuevri@student.telkomuniversity.ac.id>
2022-03-25 10:10:05 +0700

    Add project files.

---
+++ TP5-1302204069/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5_1302204069
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			// Demo class SayaTubeVideo
+			SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – bayu evri saputra");
+			video.IncreasePlayCount(7230);
+			video.PrintVideoDetails();
+
+			// Test title video di set null
+			video.SetTitle(null);
+
+			// Test IncreasePlayCount 10.000.000
+			video.IncreasePlayCount(10000001);
+			video.PrintVideoDetails();
+
+			// Test untuk jika kapasitas variabel melebihi batas
+			for (int i = 0; i < 218; i++)
+			{
+				video.IncreasePlayCount(9999999);
+			}
+			video.PrintVideoDetails();
+		}
+    }
+}
---
+++ TP5-1302204069/SayaTubeVideo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP5_1302204069
+{
+    internal class SayaTubeVideo
+    {
+		private int id;
+		private string title;
+		private int playCount;
+
+		// Deklarasi konstruktor dengan title
+		public SayaTubeVideo(string title)
+		{
+			SetTitle(title);
+			this.id = new Random().Next(10000, 99999);
+			this.playCount = 0;
+		}
+
+		public void SetTitle(string title)
+		{
+			if (title == null) return;
+			if (title.Length > 100) return;
+
+			this.title = title;
+		}
+
+		// Tambahkan playCount dengan parameter add
+		public void IncreasePlayCount(int add)
+		{
+			if (add > 10000000) return;
+
+			try
+			{
+				this.playCount = checked(this.playCount + add);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		// Tampilkan informasi video
+		public void PrintVideoDetails()
+		{
+			Console.WriteLine("Id: " + id + ", Title: " + title + ", Play: " + playCount);
+		}
+	}
+}
